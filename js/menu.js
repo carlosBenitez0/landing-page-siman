1278fbd32214319caa10f1f22d9a4744728dd0eb
Carlos Benítez <cb2724136@gmail.com>
2024-08-03 05:13:04 -0600

    feat(mobile): Add dropdown menu for mobile devices,  multimedia folder and new icons

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+const nav_hamburguer = document.querySelector(".nav__hamburguer");
+const nav_overlay = document.querySelector(".nav__overlay");
+const nav_menu = document.querySelector(".nav__menu");
+
+nav_hamburguer.addEventListener("click", () => {
+  nav_hamburguer.classList.toggle("nav__hamburguer--open");
+  nav_overlay.classList.toggle("nav__overlay--show");
+  nav_menu.classList.toggle("nav__menu--show");
+});
